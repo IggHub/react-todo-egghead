8843952bf617fcea1aa033854a0aba7cd09677f8
Iggy <igoririanto@rocketmail.com>
2017-02-17 14:12:55 -0800

    create basic input and css

--- src/App.js
+++ src/App.js
@@ -8,11 +8,20 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>React TODOS</h2>
         </div>
-        <p className="App-intro">
-          Hello, React!
-        </p>
+        <div className="todo-app">
+          <form>
+            <input type="text" />
+          </form>
+          <div className="todo-list">
+            <ul>
+              <li><input type="checkbox" value={true} />Learn JSX</li>
+              <li><input type="checkbox" value={true} />Build it!</li>
+              <li><input type="checkbox" value={true} />Ship it!!!</li>
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
