0f8d00257ce16803ab5e3cbadd47b6cf190f311c
Iggy <igoririanto@rocketmail.com>
2017-02-20 12:45:46 -0800

    add pipe function

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,1 +1,9 @@
 export const partial = (fn, ...args) => fn.bind(null, ...args)
+
+const _pipe = (fn1, fn2) => (...args) => {
+  return fn2(fn1(...args))
+}
+
+export const pipe = (...fns) => {
+  return fns.reduce(_pipe)
+}
