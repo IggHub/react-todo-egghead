33e8f1b54284f1fa57b8df56d2eef029885ffc4d
Iggy <igoririanto@rocketmail.com>
2017-02-20 16:09:58 -0800

    add onpopstate to allow back and forward button

--- src/Components/router/Router.js
+++ src/Components/router/Router.js
@@ -27,6 +27,11 @@ export class Router extends Component{
     }
   }
 
+  componentDidMount(){
+    window.onpopstate = () => {
+      this.setState({route: getCurrentPath()})
+    }
+  }
   render(){
     return(
       <div>
